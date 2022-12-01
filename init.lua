dc793c2cc021e013bda420e876a253e096756ffb
codingdestro <117741944+codingdestro@users.noreply.github.com>
2022-12-01 12:31:57 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -30,7 +30,12 @@ vim.opt.path:append { '**' } -- Finding files - Search down into subfolders
 vim.opt.wildignore:append { '*/node_modules/*' }
 
 --colorscheme
-vim.cmd([[colorscheme jellybeans]])
+function themes()
+  vim.cmd([[colorscheme jellybeans]])
+end
+if pcall(themes)then
+  print("run command :PackerUpdate")
+end
 
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
